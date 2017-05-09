f4a49cfbd21c752ffa395db176a6d1428e52a1c5
andre <brener.andre@gmail.com>
2017-05-09 19:07:45 -0300

    update

--- cryptocurrency_data.py
+++ cryptocurrency_data.py
@@ -2,34 +2,15 @@
 #          File: cryptocurrency_data.py
 #        Author: Andre Brener
 #       Created: 05 May 2017
-# Last Modified: 06 May 2017
+# Last Modified: 09 May 2017
 #   Description: description
 # =============================================================================
 import json
-import time
 import pickle
 
 from operator import itemgetter
 
 import requests
-import pandas as pd
-
-
-def get_coin_list():
-
-    coin_list_url = 'https://www.cryptocompare.com/api/data/coinlist/'
-
-    response_text = requests.get(coin_list_url).text
-
-    d = json.loads(response_text)
-    data = d['Data']
-
-    coin_list = set([
-        val['Name'] for val in data.values()
-        if all(kw not in val['Name'] for kw in ['*', ' '])
-    ])
-
-    return coin_list
 
 
 def get_price_one_by_one(coin_list):
@@ -75,7 +56,7 @@ def get_coin_price(coin_list, all_together=True):
 
 
 def save_pickle(price_dict):
-    
+
     prices = []
 
     for coin, val in price_dict:
@@ -86,39 +67,10 @@ def save_pickle(price_dict):
         pickle.dump(prices, f)
 
 
-def get_price_history(coin_list, end_date, days_past, price_type):
-    ts = time.mktime(end_date.timetuple())
-    df_list = []
-    for l in coin_list:
-        print(l)
-        url = 'https://min-api.cryptocompare.com/data/histoday?fsym={}&tsym=USD&toTs={}&limit={}'.format(
-            l, ts, days_past)
-        response_text = requests.get(url).text
-        d = json.loads(response_text)
-
-        df = pd.DataFrame(d['Data'])[['time', price_type]]
-        df['coin'] = l
-        df_list.append(df)
-    total_df = pd.concat(df_list)
-    total_df = total_df.pivot(
-        index='time', columns='coin', values=price_type).reset_index()
-    total_df['day'] = pd.to_datetime(total_df['time'], unit='s')
-    return total_df
-
-
 if __name__ == '__main__':
-    from datetime import date
-
     # coin_list = get_coin_list()
 
     coin_list = ['ETH', 'BTC']
-    end_date = date(2017, 4, 1)
-    days_past = 7
-    price_type = 'close'
 
     price_dict = get_coin_price(coin_list, True)
     save_pickle(price_dict)
-
-    # df = get_price_history(coin_list, end_date, days_past, price_type)
-
-    # print(df)
